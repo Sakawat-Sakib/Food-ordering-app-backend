c5ae272f35b5e2743e29cc3f0e146f630647528e
sakawat-sakib <mdsakawatsakib@gmail.com>
2024-11-24 11:41:47 +0600

    completed

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,39 @@
+import { v2 as cloudinary } from 'cloudinary';
 import express, {Request, Response} from "express"
 import cors from "cors"
 import mongoose from "mongoose"
 import "dotenv/config"
 import myUserRoute from './routes/MyUserRoutes'
+import myRestaurantRoute from './routes/MyRestaurantRoute'
+import restaurantRoute from './routes/RestaurantRoute'
+import orderRoute from './routes/OrderRoutes'
 
 mongoose.connect(process.env.DATABASE_URL as string).then(()=>{
     console.log("connected to database")
 })
 
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET  
+});
+
+
 const app = express()
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(cors())
+app.use("/api/order/checkout/webhook", express.raw({type: "*/*"})) // this for stripe webhook 
+app.use(express.json()) // this need to be here
+
 
 app.use("/health", (req:Request, res: Response)=>{
     res.send({message: "Health ok!"})
 })
+
 app.use("/api/my/user",myUserRoute)
+app.use("/api/my/restaurant", myRestaurantRoute)
+app.use("/api/restaurant",restaurantRoute)
+app.use("/api/order",orderRoute)
 
 app.listen(8000,()=>{
     console.log("Server Started")
--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -15,3 +15,60 @@ export const validateMyUserRequest = [
     body("country").isString().notEmpty().withMessage("Country must be string"),
     handleValidationErrors,
 ]
+
+// export const validateMyRestaurantRequest = [
+//     body("restaurantName").isString().notEmpty().withMessage("Restaurant name is requierd"),
+//     body("city").isString().notEmpty().withMessage("City name is requierd"),
+//     body("country").isString().notEmpty().withMessage("Country is requierd"),
+//     body("deliveryPrice").isFloat({min:0}).withMessage("Delivery price must be a positive number"),
+//     body("estimatedDeliveryTime").isInt({min:0}).withMessage("Estimated delivery time must be a positive number"),
+//     body("cuisines").isArray().withMessage("cuisines must be an array").not().isEmpty().withMessage("Cuisines array cannot be empty"),
+//     body("menuItems").isArray().withMessage("Menu items must be an array"),
+//     body("menuItems.*.name").notEmpty().withMessage("Menu item name is requierd"),
+//     body("menuItems.*.price").isFloat({min: 0}).withMessage("Menu item price must be a postive number"),
+//     handleValidationErrors,
+// ]
+
+export const validateMyRestaurantRequest = [
+    body("restaurantName")
+      .notEmpty()
+      .withMessage("Restaurant name is required"),
+    
+    body("city")
+      .notEmpty()
+      .withMessage("City is required"),
+    
+    body("country")
+      .notEmpty()
+      .withMessage("Country is required"),
+    
+    body("deliveryPrice")
+      .isFloat({ min: 0 })
+      .withMessage("Delivery price must be a positive number"),
+    
+    body("estimatedDeliveryTime")
+      .isInt({ min: 0 })
+      .withMessage("Estimated delivery time must be a positive integer"),
+    
+    body("cuisines")
+      .isArray()
+      .withMessage("Cuisines must be an array")
+      .not()
+      .isEmpty()
+      .withMessage("Cuisines array cannot be empty"),
+    
+    body("menuItems")
+      .isArray()
+      .withMessage("Menu items must be an array"),
+    
+    body("menuItems.*.name")
+      .notEmpty()
+      .withMessage("Menu item name is required"),
+    
+    body("menuItems.*.price")
+      .isFloat({ min: 0 })
+      .withMessage("Menu item price is required and must be a positive number"),
+    
+    handleValidationErrors,
+  ];
+  
